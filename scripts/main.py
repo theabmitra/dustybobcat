deacb8baa0d795ca6d3775eafd1cab06229ebac7
abmitra <abmitra@cisco.com>
2021-02-10 01:04:32 -0800

    Updated with currency example

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,16 @@
 import requests
-def hello():
-    print("Hello world !!\n")
-    x = requests.get('https://github.com/theabmitra')
-    print(x.text)
+from bs4 import BeautifulSoup
+
+def desplayCurrency(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    ratelist = soup.findAll("table", {"class": "ratesTable"})[0].findAll("tbody")
+    for tableVal in ratelist:
+        trList = tableVal.findAll('tr')
+        for trVal in trList[:6]:
+            print(trVal.text)
+
 
 if __name__ == "__main__":
-    hello()
+    URL = "https://www.x-rates.com/table/?from=USD&amount=1"
+    desplayCurrency(URL)
